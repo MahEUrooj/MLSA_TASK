2cbef0b1768da571a5c246a92026b4f938da636a
MAH E UROOJ <MAHE.UROOJ@STUDENTAMBASSADORS.COM>
2024-03-01 13:57:07 +0500

    set homepage for github pages deploy

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/Comp Table.jpeg";
+import image from "../images/socials/Comp Table.jpeg";
 
 const imageAltText = "desktop with books, laptop, and coffee cup";
 
