474ffb17ca0ce4d576466a9ce3bc889ae6007c28
MAH E UROOJ <murooj158@gmail.com>
2024-03-03 18:27:30 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27090,12 +27090,11 @@ var _stylesCss = require("./styles.css");
     name: "MAH E UROOJ",
     title: "JUNIOR ELEMENTARY SCHOOL TEACHER",
     email: "mahe.urooj@studentambassadors.com",
-    gitHub: "www.github.com/MahEUrooj",
+    gitHub: "https://www.github.com/MahEUrooj",
     instagram: "",
-    linkedIn: "www.linkedin.com/in/maheurooj-15a4081ab",
-    medium: "",
-    twitter: "www.twitter.com/urooj_mah",
-    youTube: "www.youtube.com/channel/UC_G8hXu5fmse7WbJsx14ySA"
+    linkedIn: "https://www.linkedin.com/in/maheurooj-15a4081ab",
+    twitter: "https://www.twitter.com/urooj_mah",
+    youTube: "https://www.youtube.com/channel/UC_G8hXu5fmse7WbJsx14ySA"
 };
 const primaryColor = "#4E567E";
 const secondaryColor = "#D2F1E4";
@@ -27105,7 +27104,7 @@ const App = ()=>{
         children: [
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _headerDefault.default), {}, void 0, false, {
                 fileName: "src/App.jsx",
-                lineNumber: 44,
+                lineNumber: 43,
                 columnNumber: 7
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _homeDefault.default), {
@@ -27113,17 +27112,17 @@ const App = ()=>{
                 title: siteProps.title
             }, void 0, false, {
                 fileName: "src/App.jsx",
-                lineNumber: 45,
+                lineNumber: 44,
                 columnNumber: 7
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _aboutDefault.default), {}, void 0, false, {
                 fileName: "src/App.jsx",
-                lineNumber: 46,
+                lineNumber: 45,
                 columnNumber: 7
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _portfolioDefault.default), {}, void 0, false, {
                 fileName: "src/App.jsx",
-                lineNumber: 47,
+                lineNumber: 46,
                 columnNumber: 7
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _footerDefault.default), {
@@ -27132,13 +27131,13 @@ const App = ()=>{
                 secondaryColor: secondaryColor
             }, void 0, false, {
                 fileName: "src/App.jsx",
-                lineNumber: 48,
+                lineNumber: 47,
                 columnNumber: 7
             }, undefined)
         ]
     }, void 0, true, {
         fileName: "src/App.jsx",
-        lineNumber: 43,
+        lineNumber: 42,
         columnNumber: 5
     }, undefined);
 };
