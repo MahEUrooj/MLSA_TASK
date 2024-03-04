5e33cd0e2880ff4b062c356c2786ac54e3228c1c
MAH E UROOJ <murooj158@gmail.com>
2024-03-04 10:51:14 +0000

    set homepage for github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const siteProps = {
   instagram: "",
   linkedIn: "maheurooj-15a4081ab",
   twitter: "MahEUrooj",
-  youTube: "KNOWLEDGE WAVE! 🎓",
+  youTube: "KNOWLEDGE WAVE!",
 };
 
 const primaryColor = "#4E567E";
